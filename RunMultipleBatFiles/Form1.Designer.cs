25ca4cc908e3545990e3c8aeb0e15eae240b40c3
WAKU-TAKE-A <waku-take-a@ymail.ne.jp>
2023-01-08 17:50:37 +0900

    1st

---
+++ RunMultipleBatFiles/Form1.Designer.cs
@@ -0,0 +1,394 @@
+﻿namespace RunMultipleBatFiles
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtStdOut = new System.Windows.Forms.TextBox();
+            this.txtBatMain = new System.Windows.Forms.TextBox();
+            this.tabCntrl = new System.Windows.Forms.TabControl();
+            this.pageList = new System.Windows.Forms.TabPage();
+            this.pageStdOut = new System.Windows.Forms.TabPage();
+            this.pageSettings = new System.Windows.Forms.TabPage();
+            this.bttnRun = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtVar01 = new System.Windows.Forms.TextBox();
+            this.txtValue01 = new System.Windows.Forms.TextBox();
+            this.txtValue02 = new System.Windows.Forms.TextBox();
+            this.txtVar02 = new System.Windows.Forms.TextBox();
+            this.txtValue03 = new System.Windows.Forms.TextBox();
+            this.txtVar03 = new System.Windows.Forms.TextBox();
+            this.txtValue04 = new System.Windows.Forms.TextBox();
+            this.txtVar04 = new System.Windows.Forms.TextBox();
+            this.txtValue05 = new System.Windows.Forms.TextBox();
+            this.txtVar05 = new System.Windows.Forms.TextBox();
+            this.txtValue06 = new System.Windows.Forms.TextBox();
+            this.txtVar06 = new System.Windows.Forms.TextBox();
+            this.txtValue07 = new System.Windows.Forms.TextBox();
+            this.txtVar07 = new System.Windows.Forms.TextBox();
+            this.txtValue08 = new System.Windows.Forms.TextBox();
+            this.txtVar08 = new System.Windows.Forms.TextBox();
+            this.txtValue09 = new System.Windows.Forms.TextBox();
+            this.txtVar09 = new System.Windows.Forms.TextBox();
+            this.txtValue10 = new System.Windows.Forms.TextBox();
+            this.txtVar10 = new System.Windows.Forms.TextBox();
+            this.tabCntrl.SuspendLayout();
+            this.pageList.SuspendLayout();
+            this.pageStdOut.SuspendLayout();
+            this.pageSettings.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // txtStdOut
+            // 
+            this.txtStdOut.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.txtStdOut.Location = new System.Drawing.Point(3, 3);
+            this.txtStdOut.Multiline = true;
+            this.txtStdOut.Name = "txtStdOut";
+            this.txtStdOut.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.txtStdOut.Size = new System.Drawing.Size(553, 324);
+            this.txtStdOut.TabIndex = 0;
+            this.txtStdOut.Text = "---StdOut---";
+            // 
+            // txtBatMain
+            // 
+            this.txtBatMain.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.txtBatMain.Location = new System.Drawing.Point(3, 3);
+            this.txtBatMain.Multiline = true;
+            this.txtBatMain.Name = "txtBatMain";
+            this.txtBatMain.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.txtBatMain.Size = new System.Drawing.Size(553, 324);
+            this.txtBatMain.TabIndex = 1;
+            this.txtBatMain.Text = "---List---";
+            // 
+            // tabCntrl
+            // 
+            this.tabCntrl.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tabCntrl.Controls.Add(this.pageList);
+            this.tabCntrl.Controls.Add(this.pageStdOut);
+            this.tabCntrl.Controls.Add(this.pageSettings);
+            this.tabCntrl.Font = new System.Drawing.Font("MS UI Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.tabCntrl.Location = new System.Drawing.Point(0, 12);
+            this.tabCntrl.Name = "tabCntrl";
+            this.tabCntrl.SelectedIndex = 0;
+            this.tabCntrl.Size = new System.Drawing.Size(567, 357);
+            this.tabCntrl.TabIndex = 2;
+            // 
+            // pageList
+            // 
+            this.pageList.Controls.Add(this.txtBatMain);
+            this.pageList.Location = new System.Drawing.Point(4, 23);
+            this.pageList.Name = "pageList";
+            this.pageList.Padding = new System.Windows.Forms.Padding(3);
+            this.pageList.Size = new System.Drawing.Size(559, 330);
+            this.pageList.TabIndex = 1;
+            this.pageList.Text = "List";
+            this.pageList.UseVisualStyleBackColor = true;
+            // 
+            // pageStdOut
+            // 
+            this.pageStdOut.Controls.Add(this.txtStdOut);
+            this.pageStdOut.Location = new System.Drawing.Point(4, 23);
+            this.pageStdOut.Name = "pageStdOut";
+            this.pageStdOut.Padding = new System.Windows.Forms.Padding(3);
+            this.pageStdOut.Size = new System.Drawing.Size(559, 330);
+            this.pageStdOut.TabIndex = 3;
+            this.pageStdOut.Text = "StdOut";
+            this.pageStdOut.UseVisualStyleBackColor = true;
+            // 
+            // pageSettings
+            // 
+            this.pageSettings.Controls.Add(this.panel1);
+            this.pageSettings.Location = new System.Drawing.Point(4, 23);
+            this.pageSettings.Name = "pageSettings";
+            this.pageSettings.Padding = new System.Windows.Forms.Padding(3);
+            this.pageSettings.Size = new System.Drawing.Size(559, 330);
+            this.pageSettings.TabIndex = 4;
+            this.pageSettings.Text = "Settings";
+            this.pageSettings.UseVisualStyleBackColor = true;
+            // 
+            // bttnRun
+            // 
+            this.bttnRun.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.bttnRun.Location = new System.Drawing.Point(4, 371);
+            this.bttnRun.Name = "bttnRun";
+            this.bttnRun.Size = new System.Drawing.Size(559, 25);
+            this.bttnRun.TabIndex = 3;
+            this.bttnRun.Text = "Run";
+            this.bttnRun.UseVisualStyleBackColor = true;
+            this.bttnRun.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.AutoScroll = true;
+            this.panel1.Controls.Add(this.txtValue10);
+            this.panel1.Controls.Add(this.txtVar10);
+            this.panel1.Controls.Add(this.txtValue09);
+            this.panel1.Controls.Add(this.txtVar09);
+            this.panel1.Controls.Add(this.txtValue08);
+            this.panel1.Controls.Add(this.txtVar08);
+            this.panel1.Controls.Add(this.txtValue07);
+            this.panel1.Controls.Add(this.txtVar07);
+            this.panel1.Controls.Add(this.txtValue06);
+            this.panel1.Controls.Add(this.txtVar06);
+            this.panel1.Controls.Add(this.txtValue05);
+            this.panel1.Controls.Add(this.txtVar05);
+            this.panel1.Controls.Add(this.txtValue04);
+            this.panel1.Controls.Add(this.txtVar04);
+            this.panel1.Controls.Add(this.txtValue03);
+            this.panel1.Controls.Add(this.txtVar03);
+            this.panel1.Controls.Add(this.txtValue02);
+            this.panel1.Controls.Add(this.txtVar02);
+            this.panel1.Controls.Add(this.txtValue01);
+            this.panel1.Controls.Add(this.txtVar01);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(3, 3);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(553, 324);
+            this.panel1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(2, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(64, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "VARIABLE";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(208, 13);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(45, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "VALUE";
+            // 
+            // txtVar01
+            // 
+            this.txtVar01.Location = new System.Drawing.Point(5, 29);
+            this.txtVar01.Name = "txtVar01";
+            this.txtVar01.Size = new System.Drawing.Size(200, 20);
+            this.txtVar01.TabIndex = 2;
+            // 
+            // txtValue01
+            // 
+            this.txtValue01.Location = new System.Drawing.Point(211, 29);
+            this.txtValue01.Name = "txtValue01";
+            this.txtValue01.Size = new System.Drawing.Size(320, 20);
+            this.txtValue01.TabIndex = 3;
+            // 
+            // txtValue02
+            // 
+            this.txtValue02.Location = new System.Drawing.Point(211, 55);
+            this.txtValue02.Name = "txtValue02";
+            this.txtValue02.Size = new System.Drawing.Size(320, 20);
+            this.txtValue02.TabIndex = 5;
+            // 
+            // txtVar02
+            // 
+            this.txtVar02.Location = new System.Drawing.Point(5, 55);
+            this.txtVar02.Name = "txtVar02";
+            this.txtVar02.Size = new System.Drawing.Size(200, 20);
+            this.txtVar02.TabIndex = 4;
+            // 
+            // txtValue03
+            // 
+            this.txtValue03.Location = new System.Drawing.Point(211, 81);
+            this.txtValue03.Name = "txtValue03";
+            this.txtValue03.Size = new System.Drawing.Size(320, 20);
+            this.txtValue03.TabIndex = 7;
+            // 
+            // txtVar03
+            // 
+            this.txtVar03.Location = new System.Drawing.Point(5, 81);
+            this.txtVar03.Name = "txtVar03";
+            this.txtVar03.Size = new System.Drawing.Size(200, 20);
+            this.txtVar03.TabIndex = 6;
+            // 
+            // txtValue04
+            // 
+            this.txtValue04.Location = new System.Drawing.Point(211, 107);
+            this.txtValue04.Name = "txtValue04";
+            this.txtValue04.Size = new System.Drawing.Size(320, 20);
+            this.txtValue04.TabIndex = 9;
+            // 
+            // txtVar04
+            // 
+            this.txtVar04.Location = new System.Drawing.Point(5, 107);
+            this.txtVar04.Name = "txtVar04";
+            this.txtVar04.Size = new System.Drawing.Size(200, 20);
+            this.txtVar04.TabIndex = 8;
+            // 
+            // txtValue05
+            // 
+            this.txtValue05.Location = new System.Drawing.Point(211, 133);
+            this.txtValue05.Name = "txtValue05";
+            this.txtValue05.Size = new System.Drawing.Size(320, 20);
+            this.txtValue05.TabIndex = 11;
+            // 
+            // txtVar05
+            // 
+            this.txtVar05.Location = new System.Drawing.Point(5, 133);
+            this.txtVar05.Name = "txtVar05";
+            this.txtVar05.Size = new System.Drawing.Size(200, 20);
+            this.txtVar05.TabIndex = 10;
+            // 
+            // txtValue06
+            // 
+            this.txtValue06.Location = new System.Drawing.Point(211, 159);
+            this.txtValue06.Name = "txtValue06";
+            this.txtValue06.Size = new System.Drawing.Size(320, 20);
+            this.txtValue06.TabIndex = 13;
+            // 
+            // txtVar06
+            // 
+            this.txtVar06.Location = new System.Drawing.Point(5, 159);
+            this.txtVar06.Name = "txtVar06";
+            this.txtVar06.Size = new System.Drawing.Size(200, 20);
+            this.txtVar06.TabIndex = 12;
+            // 
+            // txtValue07
+            // 
+            this.txtValue07.Location = new System.Drawing.Point(211, 185);
+            this.txtValue07.Name = "txtValue07";
+            this.txtValue07.Size = new System.Drawing.Size(320, 20);
+            this.txtValue07.TabIndex = 15;
+            // 
+            // txtVar07
+            // 
+            this.txtVar07.Location = new System.Drawing.Point(5, 185);
+            this.txtVar07.Name = "txtVar07";
+            this.txtVar07.Size = new System.Drawing.Size(200, 20);
+            this.txtVar07.TabIndex = 14;
+            // 
+            // txtValue08
+            // 
+            this.txtValue08.Location = new System.Drawing.Point(211, 211);
+            this.txtValue08.Name = "txtValue08";
+            this.txtValue08.Size = new System.Drawing.Size(320, 20);
+            this.txtValue08.TabIndex = 17;
+            // 
+            // txtVar08
+            // 
+            this.txtVar08.Location = new System.Drawing.Point(5, 211);
+            this.txtVar08.Name = "txtVar08";
+            this.txtVar08.Size = new System.Drawing.Size(200, 20);
+            this.txtVar08.TabIndex = 16;
+            // 
+            // txtValue09
+            // 
+            this.txtValue09.Location = new System.Drawing.Point(211, 237);
+            this.txtValue09.Name = "txtValue09";
+            this.txtValue09.Size = new System.Drawing.Size(320, 20);
+            this.txtValue09.TabIndex = 19;
+            // 
+            // txtVar09
+            // 
+            this.txtVar09.Location = new System.Drawing.Point(5, 237);
+            this.txtVar09.Name = "txtVar09";
+            this.txtVar09.Size = new System.Drawing.Size(200, 20);
+            this.txtVar09.TabIndex = 18;
+            // 
+            // txtValue10
+            // 
+            this.txtValue10.Location = new System.Drawing.Point(211, 263);
+            this.txtValue10.Name = "txtValue10";
+            this.txtValue10.Size = new System.Drawing.Size(320, 20);
+            this.txtValue10.TabIndex = 21;
+            // 
+            // txtVar10
+            // 
+            this.txtVar10.Location = new System.Drawing.Point(5, 263);
+            this.txtVar10.Name = "txtVar10";
+            this.txtVar10.Size = new System.Drawing.Size(200, 20);
+            this.txtVar10.TabIndex = 20;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(567, 398);
+            this.Controls.Add(this.bttnRun);
+            this.Controls.Add(this.tabCntrl);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.tabCntrl.ResumeLayout(false);
+            this.pageList.ResumeLayout(false);
+            this.pageList.PerformLayout();
+            this.pageStdOut.ResumeLayout(false);
+            this.pageStdOut.PerformLayout();
+            this.pageSettings.ResumeLayout(false);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtStdOut;
+        private System.Windows.Forms.TextBox txtBatMain;
+        private System.Windows.Forms.TabControl tabCntrl;
+        private System.Windows.Forms.TabPage pageList;
+        private System.Windows.Forms.TabPage pageStdOut;
+        private System.Windows.Forms.TabPage pageSettings;
+        private System.Windows.Forms.Button bttnRun;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TextBox txtValue10;
+        private System.Windows.Forms.TextBox txtVar10;
+        private System.Windows.Forms.TextBox txtValue09;
+        private System.Windows.Forms.TextBox txtVar09;
+        private System.Windows.Forms.TextBox txtValue08;
+        private System.Windows.Forms.TextBox txtVar08;
+        private System.Windows.Forms.TextBox txtValue07;
+        private System.Windows.Forms.TextBox txtVar07;
+        private System.Windows.Forms.TextBox txtValue06;
+        private System.Windows.Forms.TextBox txtVar06;
+        private System.Windows.Forms.TextBox txtValue05;
+        private System.Windows.Forms.TextBox txtVar05;
+        private System.Windows.Forms.TextBox txtValue04;
+        private System.Windows.Forms.TextBox txtVar04;
+        private System.Windows.Forms.TextBox txtValue03;
+        private System.Windows.Forms.TextBox txtVar03;
+        private System.Windows.Forms.TextBox txtValue02;
+        private System.Windows.Forms.TextBox txtVar02;
+        private System.Windows.Forms.TextBox txtValue01;
+        private System.Windows.Forms.TextBox txtVar01;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ RunMultipleBatFiles/Form1.cs
@@ -0,0 +1,175 @@
+﻿// DOSコマンドを実行し出力データを取得する
+// https://dobon.net/vb/dotnet/process/standardoutput.html
+// 環境変数の値を取得するには？［C#／VB］
+// https://atmarkit.itmedia.co.jp/ait/articles/1803/07/news023.html
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Text.RegularExpressions;
+using System.IO;
+using Microsoft.Win32;
+using System.Threading;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace RunMultipleBatFiles
+{
+    public partial class Form1 : Form
+    {
+        //定数
+        private readonly string FN_BAT_MAIN = System.AppDomain.CurrentDomain.BaseDirectory + "List_Bat_Main.txt";
+
+        //Processオブジェクトを作成
+        private System.Diagnostics.Process proc = null;
+
+        //環境変数リスト
+        Dictionary<System.Windows.Forms.TextBox, System.Windows.Forms.TextBox> lstEnvVar = new Dictionary<System.Windows.Forms.TextBox, System.Windows.Forms.TextBox>();
+
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            lstEnvVar.Add(txtVar01, txtValue01);
+            lstEnvVar.Add(txtVar02, txtValue02);
+            lstEnvVar.Add(txtVar03, txtValue03);
+            lstEnvVar.Add(txtVar04, txtValue04);
+            lstEnvVar.Add(txtVar05, txtValue05);
+            lstEnvVar.Add(txtVar06, txtValue06);
+            lstEnvVar.Add(txtVar07, txtValue07);
+            lstEnvVar.Add(txtVar08, txtValue08);
+            lstEnvVar.Add(txtVar09, txtValue09);
+            lstEnvVar.Add(txtVar10, txtValue10);
+
+            tabCntrl.SelectedTab = pageList;
+            init();
+            newProc();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            txtStdOut.Text = "";
+            tabCntrl.SelectedTab = pageStdOut;
+            Application.DoEvents();
+
+            Task runMultipleBatAsync = Task.Run(() => {
+                if (this.InvokeRequired)
+                    this.Invoke((MethodInvoker)(() => RunMultipleBat()));
+                else
+                    RunMultipleBat();
+            });
+
+            bttnRun.Enabled = false;
+        }
+
+        private void CheckFileAndSetTextBox(string filename, System.Windows.Forms.TextBox obj)
+        {
+            if (File.Exists(filename))
+            {
+                using (StreamReader sr = new StreamReader(filename, Encoding.GetEncoding("utf-8")))
+                    obj.Text = sr.ReadToEnd();
+            }
+            else
+            {
+                obj.Text = "";
+
+                using (FileStream fs = File.Create(filename))
+                    fs.Close();
+            }
+        }
+
+        private void newProc()
+        {
+            proc = new System.Diagnostics.Process();
+
+            //ComSpec(cmd.exe)のパスを取得して、FileNameプロパティに指定
+            proc.StartInfo.FileName = System.Environment.GetEnvironmentVariable("ComSpec");
+
+            //出力を読み取れるようにする
+            proc.StartInfo.UseShellExecute = false;
+            proc.StartInfo.RedirectStandardOutput = true;
+            proc.StartInfo.RedirectStandardInput = false;
+
+            //ウィンドウを表示しないようにする
+            proc.StartInfo.CreateNoWindow = true;
+        }
+
+        private void init()
+        {
+            //ファイルのチェックとテキストボックスの初期化
+            CheckFileAndSetTextBox(FN_BAT_MAIN, txtBatMain);
+            txtStdOut.Text = "";
+        }
+
+        private void runCommand(string cmd)
+        {
+            proc.StartInfo.Arguments = @"/c " + cmd;
+            proc.Start();
+            txtStdOut.Text += proc.StandardOutput.ReadToEnd();
+            proc.WaitForExit();
+            proc.Close();
+        }
+
+        private void RunMultipleBat()
+        {
+            try
+            {
+                //改行で分割
+                string[] lst_bat_main = string.IsNullOrWhiteSpace(txtBatMain.Text) ? null : Regex.Split(txtBatMain.Text, "\r\n");
+
+                // Mainを1行づつ実行
+                if (lst_bat_main != null)
+                {
+                    foreach (var cmd_main in lst_bat_main)
+                    {
+                        string systemPath = Environment.GetEnvironmentVariable("path", EnvironmentVariableTarget.Machine);
+                        string userPath = Environment.GetEnvironmentVariable("path", EnvironmentVariableTarget.User);
+                        Environment.SetEnvironmentVariable("path", systemPath + ";" + userPath, EnvironmentVariableTarget.Process);
+                        //デバッグ用
+                        //string procPath = Environment.GetEnvironmentVariable("path", EnvironmentVariableTarget.Process);
+
+                        //★★★環境変数の設定
+                        foreach (var envVar in lstEnvVar)
+                        {
+                            if (!string.IsNullOrEmpty(envVar.Key.Text))
+                            {
+                                Environment.SetEnvironmentVariable(envVar.Key.Text, envVar.Value.Text, EnvironmentVariableTarget.Process);
+                            }
+                        }
+
+                        //処理
+                        if (!string.IsNullOrWhiteSpace(cmd_main))
+                        {
+                            txtStdOut.Text += "\r\n>> " + cmd_main + "\r\n";
+                            runCommand(cmd_main);
+                        }
+
+                        txtStdOut.SelectionStart = txtStdOut.Text.Length;
+                        txtStdOut.Focus();
+                        txtStdOut.ScrollToCaret();
+
+                        Application.DoEvents();
+                    }
+                }
+                else
+                {
+                    txtStdOut.Text = "Mainは空です";
+                }
+            }
+            catch (Exception ex)
+            {
+                txtStdOut.Text += "\r\nException:r\n" + ex.ToString() + "\r\n";
+            }
+            finally
+            {
+                bttnRun.Enabled = true;
+            }
+        }
+    }
+}
